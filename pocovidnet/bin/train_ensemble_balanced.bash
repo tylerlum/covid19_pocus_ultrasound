346dcb0b8801f4db134539f9b327509d126eb58b
Tyler Lum <tylergwlum@gmail.com>
2020-10-20 15:47:20 -0700

    Save train_ensemble scripts

---
+++ pocovidnet/bin/train_ensemble_balanced.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Run from covid19_pocus_ultrasound/pocovidnet directory. Trains many models using 3 folds for training, 1 fold for validation, and 1 fold for testing. Saves to covid19_pocus_ultrasound/pocovidnet/models/ensemble_balanced directory
+
+# Test fold 4, vgg_base
+python3 scripts/train_covid19_proper.py --trainable_base_layers 3 --model_id 'vgg_base' --learning_rate 1e-4 --data_dir '../data/cross_validation_balanced' --validation_fold 3 --test_fold 4 --epochs 40 --batch_size 8 --model_name "ensemble_balanced" --num_models 5
+python3 scripts/train_covid19_proper.py --trainable_base_layers 3 --model_id 'vgg_base' --learning_rate 1e-4 --data_dir '../data/cross_validation_balanced' --validation_fold 2 --test_fold 4 --epochs 40 --batch_size 8 --model_name "ensemble_balanced" --num_models 5
+python3 scripts/train_covid19_proper.py --trainable_base_layers 3 --model_id 'vgg_base' --learning_rate 1e-4 --data_dir '../data/cross_validation_balanced' --validation_fold 1 --test_fold 4 --epochs 40 --batch_size 8 --model_name "ensemble_balanced" --num_models 5
+python3 scripts/train_covid19_proper.py --trainable_base_layers 3 --model_id 'vgg_base' --learning_rate 1e-4 --data_dir '../data/cross_validation_balanced' --validation_fold 0 --test_fold 4 --epochs 40 --batch_size 8 --model_name "ensemble_balanced" --num_models 5
