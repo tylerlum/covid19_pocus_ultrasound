80948e84895fb621bb7256649fbd3881de3af3b7
Tyler Lum <tylergwlum@gmail.com>
2020-10-26 14:31:07 -0700

    Save new scripts

--- pocovidnet/bin/train_ensemble_balanced3_helper.bash
+++ pocovidnet/bin/train_ensemble_balanced3_helper.bash
@@ -6,12 +6,12 @@ do
     python3 scripts/train_covid19_proper.py --trainable_base_layers 3 --model_id 'vgg_base' --learning_rate 1e-4 --data_dir '../data/cross_validation_balanced' --validation_fold 2 --test_fold 3 --epochs 40 --batch_size 8 --model_name "ensemble_balanced3_part${i}" --num_models 1
 done
 
-mkdir models/ensemble_balanced3
+mkdir -p models/ensemble_balanced3/validation-fold-2_test-fold-3
 
 # Copy over files
 i=0
 all_models=$(find ./models/ -wholename "*ensemble_balanced3_part*model-0")
-for model in $all_models; do mv $model/ ./models/ensemble_balanced3/model-$i; i=$(($i+1)); done
+for model in $all_models; do mv $model/ ./models/ensemble_balanced3/validation-fold-2_test-fold-3/model-$i; i=$(($i+1)); done
 
 # Remove old folders
 old_ones=$(find ./models/ -type d -name "*ensemble_balanced3_part*")
---
+++ pocovidnet/bin/train_ensemble_balanced4.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Run from covid19_pocus_ultrasound/pocovidnet directory. Trains many models using 3 folds for training, 1 fold for validation, and 1 fold for testing. Saves to covid19_pocus_ultrasound/pocovidnet/models/ensemble_balanced4 directory
+
+for i in {1..4}
+do
+    python3 scripts/train_covid19_proper.py --trainable_base_layers 3 --model_id 'vgg_base' --learning_rate 1e-4 --data_dir '../data/cross_validation_balanced' --validation_fold 2 --test_fold 3 --epochs 1 --batch_size 8 --model_name "ensemble_balanced4_part${i}" --num_models 1
+done
+
+mkdir models/ensemble_balanced4
+
+# Copy over files
+i=0
+all_models=$(find ./models/ -wholename "*ensemble_balanced4_part*model-0")
+for model in $all_models; do mv $model/ ./models/ensemble_balanced4/model-$i; i=$(($i+1)); done
+
+# Remove old folders
+old_ones=$(find ./models/ -type d -name "*ensemble_balanced4_part*")
+for f in $old_ones; do rm -r $f; done
