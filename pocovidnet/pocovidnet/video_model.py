a74e914293621696dc093acc403deb6e5d3eaf80
Tyler Lum <tylergwlum@gmail.com>
2020-11-17 23:33:30 +0000

    Replace dropout with spatial dropout layers

--- pocovidnet/pocovidnet/video_model.py
+++ pocovidnet/pocovidnet/video_model.py
@@ -1,7 +1,7 @@
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import (
-    Activation, Conv3D, Dense, Dropout, Flatten, MaxPooling3D
+    Activation, Conv3D, Dense, Dropout, Flatten, MaxPooling3D, SpatialDropout2D
 )
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.losses import categorical_crossentropy
@@ -24,20 +24,20 @@ def get_video_model(input_shape, nb_classes, batch_norm=False):
     model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same', activation='relu'))
     if batch_norm: model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size=(3, 3, 3), padding='same'))
-    model.add(Dropout(0.5))
+    model.add(SpatialDropout2D(0.5))
 
     model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same', activation='relu'))
     if batch_norm: model.add(BatchNormalization())
     model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same', activation='relu'))
     if batch_norm: model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size=(3, 3, 3), padding='same'))
-    model.add(Dropout(0.5))
+    model.add(SpatialDropout2D(0.5))
 
     model.add(Flatten())
     model.add(Dense(2048, activation='relu'))
     if batch_norm: model.add(BatchNormalization())
     # model.add(Activation('sigmoid'))
-    model.add(Dropout(0.5))
+    model.add(SpatialDropout2D(0.5))
     model.add(Dense(nb_classes, activation='softmax'))
 
     return model
