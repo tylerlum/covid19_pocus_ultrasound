eaa0c39ce4b0f8b05072ff270436312a54d40706
Tyler Lum <tylergwlum@gmail.com>
2020-11-20 18:36:09 +0000

    Go back to original video model

--- pocovidnet/pocovidnet/video_model.py
+++ pocovidnet/pocovidnet/video_model.py
@@ -14,24 +14,23 @@ def get_video_model(input_shape, nb_classes):
     model = Sequential()
     model.add(
         Conv3D(
-            16,
-            kernel_size=(5, 3, 3),
+            32,
+            kernel_size=(3, 3, 3),
             input_shape=(input_shape),
-            # padding='same'
+            padding='same'
         )
     )
     model.add(Activation('relu'))
-    model.add(Lambda(lambda x: tensorflow.keras.backend.squeeze(x, axis=1)))
-    model.add(Conv2D(16, kernel_size=(3, 3)))
+    model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same'))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3)))
+    model.add(MaxPooling3D(pool_size=(3, 3, 3), padding='same'))
     model.add(Dropout(0.5))
 
-    model.add(Conv2D(32, kernel_size=(3, 3)))
+    model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same'))
     model.add(Activation('relu'))
-    model.add(Conv2D(32, kernel_size=(3, 3)))
+    model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same'))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3)))
+    model.add(MaxPooling3D(pool_size=(3, 3, 3), padding='same'))
     model.add(Dropout(0.5))
 
     model.add(Flatten())
