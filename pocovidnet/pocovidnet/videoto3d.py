264c79ca296f175d672f49627c424a9813687a90
Tyler Lum <tylergwlum@gmail.com>
2021-01-06 07:22:01 +0000

    Clean video to 3d code

--- pocovidnet/pocovidnet/videoto3d.py
+++ pocovidnet/pocovidnet/videoto3d.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import pickle
 import os
+import math
 
 
 class Videoto3D:
@@ -24,47 +25,47 @@ class Videoto3D:
     def video3d(self, vid_files, labels, save=None):
         # iterate to fill data
         data_3d, labels_3d, files_3d = [], [], []
-        for train_vid, train_lab in zip(vid_files, labels):
+        for train_vid, train_label in zip(vid_files, labels):
             cap = cv2.VideoCapture(os.path.join(self.vid_path, train_vid))
-            fr = cap.get(5)
-            show_every = round(fr / self.framerate)
-            print(
-                train_vid, fr, cap.get(7), "available frames:",
-                cap.get(7) / show_every
-            )
-            frames_available = cap.get(7) / show_every
-            end_is_close = frames_available % self.depth >= 4
-            number_selected = int(
-                end_is_close
-            ) + frames_available // self.depth
-            print(number_selected, cap.get(7), "show every", show_every)
-            vid_part_count = 0
+            video_framerate = cap.get(cv2.CAP_PROP_FPS)
+            video_num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+            show_every = math.ceil(video_framerate / self.framerate)  # ceil to avoid 0
+            frames_available = video_num_frames / show_every
+            video_clips_available = frames_available // self.depth
+            print(f"{train_vid}, {video_framerate} FPS, {video_num_frames} frames, show every {show_every} frames, available frames: {frames_available}, available video clips: {video_clips_available}")
+
+            video_clips_counter = 0
             current_data = []
-            # for frame_id in range(int(cap.get(7))):
+
             while cap.isOpened():
-                frame_id = cap.get(1)
+                frame_id = cap.get(cv2.CAP_PROP_POS_FRAMES)
                 ret, frame = cap.read()
                 if (ret != True):
                     break
-                # plt.imshow(image)
-                # plt.show()
+
                 image = frame if not self.grayscale else cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 image = cv2.resize(image, (self.width, self.height))
 
-                if frame_id % show_every == 0 or (
-                    end_is_close and frame_id == int(cap.get(7) - 1)
-                ):
+                # Grab image every X frames
+                if frame_id % show_every == 0:
                     current_data.append(image)
+
+                # Store video clip
                 if len(current_data) == self.depth:
                     data_3d.append(current_data)
-                    labels_3d.append(train_lab)
+                    labels_3d.append(train_label)
                     files_3d.append(train_vid)
                     current_data = []
-                    vid_part_count += 1
-                if vid_part_count >= self.max_vid[train_lab]:
-                    print("already 5 parts of this video")
+                    video_clips_counter += 1
+
+                # Got max number of clips for this video
+                if video_clips_counter >= self.max_vid[train_label]:
+                    print(f"already {video_clips_counter} clips taken from this video")
                     break
+
             cap.release()
+
         # If grayscale, still want a channel size of 1
         data = np.asarray(data_3d) if not self.grayscale else np.expand_dims(np.asarray(data_3d), 4)
         data = data / 255
