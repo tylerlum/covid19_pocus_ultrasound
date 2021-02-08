2cfa8860d85510d550d6a2668babe1398c0a82de
Tyler Lum <tylergwlum@gmail.com>
2021-02-08 11:36:51 +0000

    Fix evidential setup

--- pocovidnet/scripts/video_classification.py
+++ pocovidnet/scripts/video_classification.py
@@ -168,7 +168,6 @@ def main():
     parser.add_argument('--augment', type=str2bool, nargs='?', const=True, default=False, help='video augmentation')
     parser.add_argument('--optimizer', type=str, default="adam", help='optimizer for training')
     parser.add_argument('--pretrained_cnn', type=str, default="vgg16", help='pretrained cnn architecture')
-    parser.add_argument('--evidential', type=str2bool, nargs='?', const=True, default=False, help='evidential network')
 
     parser.add_argument('--reduce_learning_rate', type=str2bool, nargs='?', const=True, default=False,
                         help='use reduce learning rate callback')
@@ -199,6 +198,13 @@ def main():
         args.grayscale = True
         args.width, args.height = 64, 64
         print("This model requires width, height, grayscale = {args.width}, {args.height}, {args.grayscale}")
+
+    if args.architecture.endswith('evidential'):
+        evidential = True
+        print("This model uses evidential")
+    else:
+        evidential = False
+        print("This model does not use evidential")
 
     # Deterministic behavior
     set_random_seed(args.random_seed)
@@ -367,12 +373,10 @@ def main():
     class_weight = {i: sum(train_counts) / train_counts[i] for i in range(len(train_counts))}
     print(f"class_weight = {class_weight}")
 
-    if args.evidential:
-        args.architecture += "_"+ "evidential"
     model = VIDEO_MODEL_FACTORY[args.architecture](input_shape, nb_classes, args.pretrained_cnn)
     print('---------------------------model---------------------\n', args.architecture)
 
-    # tf.keras.utils.plot_model(model, os.path.join(FINAL_OUTPUT_DIR, f"{args.architecture}.png"), show_shapes=True)
+    tf.keras.utils.plot_model(model, os.path.join(FINAL_OUTPUT_DIR, f"{args.architecture}.png"), show_shapes=True)
 
     # evidential loss function
     def KL(alpha, K):
@@ -400,7 +404,7 @@ def main():
     )
 
     loss = categorical_crossentropy
-    if args.evidential:
+    if evidential:
         loss = ev_loss
 
     if args.optimizer == "adam":
